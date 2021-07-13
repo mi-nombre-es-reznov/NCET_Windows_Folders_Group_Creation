629d7e8ae0a0341ec7238c4c88b32fdeae86c286
Nicholas Perez-Aguilar <npere017@ucr.edu>
2021-07-13 00:17:23 -0700

    Add project files.

---
+++ NCET_Windows_Folders_Group_Creation/main.cpp
@@ -0,0 +1,19 @@
+/* 
+	Name: NCET_Windows_Folders_Group_Creation
+	Author: Nicholas Perez-Aguilar
+	Company: NCompEng Technologies
+	Date of Start: 13 July 2021
+*/
+
+// Pre-processors
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	cout << "Hello, World!" << endl;
+
+	system("pause");
+	return 0;
+}
