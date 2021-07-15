806f73a49ad48eea98a04b2b63fdb46470208331
Nicholas Perez-Aguilar <npere017@ucr.edu>
2021-07-15 15:25:06 -0700

    Allows for users to select a grouping choice and advertises NCompEng Technologies at the end of the program.

--- NCET_Windows_Folders_Group_Creation/main.cpp
+++ NCET_Windows_Folders_Group_Creation/main.cpp
@@ -14,16 +14,43 @@
 using namespace std;
 
 // Global Values
+string GROUPS[3] = {"Book", "New Tutee", "Monthly Gaming"};
 string TEST = "/TEST";
 string PATH;
 
 // Function Declarations
 void test_file_directory(void);
+int select_group(void);
 
 int main()
 {
-	test_file_directory();
+	// Local Variables
+	int group_choice = -1;
 
+	test_file_directory();	// Test user-given path
+	group_choice = select_group();	// User-selects folder creation option
+	system("CLS");	// Clear screen before entering options functions
+
+	// Choice mappings
+	if (group_choice == 1)
+	{
+
+	}
+	else if (group_choice == 2)
+	{
+
+	}
+	else if (group_choice == 3)
+	{
+		
+	}
+	else
+	{
+		cout << "An internal error occurred!!!\nPlease try to rerun the program after checking the code!" << endl;
+	}
+
+	system("CLS");
+	cout << "NCompEng Technologies thanks you for using NCET Windows Folders Group Creation Application." << endl;
 	system("pause");
 	return 0;
 }
@@ -67,4 +94,51 @@ void test_file_directory(void)
 			Sleep(1000);
 		}
 	}
+}
+
+// Allow a user to select which folder group they want to create
+int select_group(void)
+{
+	// Local Variables
+	int num_choices = sizeof(GROUPS) / sizeof(GROUPS[0]);
+	int valid_choice = -1;
+	char test;	// Only allows numbers 0 -> 9 for input. Convert to string if array GROUPS exceeds 10+
+
+	while (valid_choice < 1 || valid_choice > num_choices)
+	{
+		system("CLS");
+
+		for (int i = 0; i < num_choices; i++)
+		{
+			cout << (i + 1) << ") " << GROUPS[i] << endl;
+		}
+
+		cout << "\nSelect an group to begin: ";
+		cin >> test;	// Only accepts 1 character value.
+
+		// Verify input is a number
+		if (isdigit(test) == 0)
+		{
+			valid_choice = -1;
+		}
+		else
+		{
+			valid_choice = int(test);
+			valid_choice -= 48;	// Subtract out ASCII char value of 48 for numbers.
+		}
+
+		system("CLS"); // Clear screen for feedback
+
+		if (valid_choice < 1 || valid_choice > num_choices)
+		{
+			cout << "Answer choice is invalid\nEnter a valid group to begin." << endl;
+			valid_choice = -1;
+		}
+
+		Sleep(1000);
+	}
+
+	valid_choice -= 1;	// Set up to pull directly from array of GROUPS
+
+	return valid_choice;
 }
